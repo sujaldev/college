f2fc7d02e04f477f472cf7c070a0e1c349f5ee3e
Sujal Singh <email.sujalsingh@gmail.com>
2023-12-09 19:09:04 +0530

    add c practical experiment 4

---
+++ src/sem1/programming-lab/practical-file/experiments/e4.c
@@ -0,0 +1,40 @@
+// Write a C program to find the greatest number among three numbers provided by the user using if else.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    int operation;
+
+    // Read a
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    // Read b
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // Read operation
+    printf("Enter operation add(1), subtract(2), multiply(3), divide(4): ");
+    scanf("%d", &operation);
+
+    // Perform the operation input by the user on a and b
+    switch (operation) {
+        case 1:
+            printf("%d + %d = %d", a, b, a + b);
+            break;
+        case 2:
+            printf("%d - %d = %d", a, b, a - b);
+            break;
+        case 3:
+            printf("%d x %d = %d", a, b, a * b);
+            break;
+        case 4:
+            printf("%d / %d = %f", a, b, (float) a / b);
+            break;
+        default:
+            printf("Invalid operation.");
+    }
+
+    return 0;
+}
