c631351c335f76cafc1d6f25b4a28e59453e2121
Sujal Singh <email.sujalsingh@gmail.com>
2024-05-09 13:22:11 +0530

    python: q5

---
+++ src/sem2/python-lab/programs/q5.py
@@ -0,0 +1,4 @@
+# Write a program that receives input from the user to calculate the area of a square.
+
+side = int(input("Enter side length of square(m): "))
+print(f"Area of square of side length {side} is {side * side}.")
