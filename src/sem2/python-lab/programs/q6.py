2e62e4197a5cbf7468e4a9a2b0b15e9c92fedb07
Sujal Singh <email.sujalsingh@gmail.com>
2024-05-09 13:26:19 +0530

    python: q6

---
+++ src/sem2/python-lab/programs/q6.py
@@ -0,0 +1,5 @@
+# Write a program that receive input from the user to calculate the area of rectangle.
+
+length = int(input("Enter length: "))
+breadth = int(input("Enter breadth: "))
+print(f"Area of rectangle having length {length} and breadth {breadth} is {length * breadth}.")
