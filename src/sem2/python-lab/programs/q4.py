20bd3759843f462cd43d381c3413d90ab022447d
Sujal Singh <email.sujalsingh@gmail.com>
2024-05-09 13:19:23 +0530

    python: q4

---
+++ src/sem2/python-lab/programs/q4.py
@@ -0,0 +1,5 @@
+# Write a program that receives input from the user to calculate the area of a triangle.
+
+base = int(input("Enter base length of triangle(m): "))
+height = int(input("Enter height of triangle(m): "))
+print("area =", 0.5 * base * height)
